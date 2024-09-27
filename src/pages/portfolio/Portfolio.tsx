7125ddef9921ca76a0b9502f3b61ce1c9c98c620
Nito Crespi <AlvaroCrespiLiut@gmail.com>
2024-09-27 12:35:09 -0300

    Update Portfolio

--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -6,8 +6,7 @@ import CertificationsList from "../../components/CertificationsList";
 import ProjectsList from "../../components/ProjectsList";
 import useLanguage from "../../hook/useLanguage";
 import PresentationsList from "../../components/PresentationList";
-import SocialButton from "../../components/SocialButton/SocialButton";
-import DownloadButton from "../../components/DownloadButton/DownloadButton";
+import HeaderInfo from "../../components/HeaderInfo/HeaderInfo";
 
 const Portfolio = () => {
   const { isSpanish, setSpanish } = useLanguage();
@@ -24,52 +23,8 @@ const Portfolio = () => {
           </button>
         </div>
         {/*  */}
-        <div className="profile_container">
-          <img
-            src={`${process.env.PUBLIC_URL}/assets/cv/profile_photo.png`}
-            alt="Profile photo"
-            className="profile_photo"
-          />
-          <div className="profile_text">
-            <h1>Álvaro Crespi Liut</h1>
-            <p>
-              <span>
-                {isSpanish
-                  ? "Desarrollador Full-Stack | Ofreciendo soluciones de software de calidad."
-                  : "Full-Stack Developer | Delivering Quality Software Solutions."}
-              </span>
-            </p>
-            <div>
-              <SocialButton
-                url="https://www.linkedin.com/in/nito-crespi/"
-                name="LinkedIn"
-                iconPath="/assets/icons/linkedin.svg"
-                backgroundColor="#0077B5"
-              />
-              <SocialButton
-                url="https://github.com/Nito-Crespi"
-                name="GitHub"
-                iconPath="/assets/icons/github-white.svg"
-                backgroundColor="#333"
-              />
-              <DownloadButton
-                name="Curriculum Vitae"
-                iconPath="/assets/icons/document-pdf.svg"
-                filePath={
-                  isSpanish
-                    ? "/assets/files/Alvaro Crespi Liut - Currículum Vitae ES.pdf"
-                    : "/assets/files/Alvaro Crespi Liut - Currículum Vitae EN.pdf"
-                }
-                downloadName={
-                  isSpanish
-                    ? "Alvaro Crespi Liut - Currículum Vitae ES.pdf"
-                    : "Alvaro Crespi Liut - Currículum Vitae EN.pdf"
-                }
-              />
-            </div>
-          </div>
-        </div>
-        <hr color="purple" />
+        <HeaderInfo />
+        <hr />
         {/*  */}
         <h3>{isSpanish ? "Perfil personal" : "Personal profile"}</h3>
         <PresentationsList />
