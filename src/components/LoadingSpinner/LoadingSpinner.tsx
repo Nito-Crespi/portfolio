f9419a1115f471371eefa0e6d5ba1b90e73a7eef
Nito Crespi <AlvaroCrespiLiut@gmail.com>
2024-09-27 12:34:27 -0300

    Update LoadingSpinner

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './LoadingSpinner.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./LoadingSpinner.css";
 
 const LoadingSpinner: React.FC = () => {
   return (
